e14151840c2e412e82c562eaf0579754f6ca6d4c
Senuka Chandunu <senukaacc@gmail.com>
2025-04-15 22:25:49 +0530

    create 1st Accordion

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import MyAccordion from "./SHADCN/MyAccordion";
 
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <button>Click Me</button>
+    <div>
+      <MyAccordion />
     </div>
   );
 }
